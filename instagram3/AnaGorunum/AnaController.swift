2a62d4381d1c00775a20295dd26423aee24db335
Fırat Demir <dferhat263@gmail.com>
2024-05-18 19:02:40 +0300

    Ana Ekranı Tasarlamaya Giriş

---
+++ instagram3/AnaGorunum/AnaController.swift
@@ -0,0 +1,8 @@
+//
+//  AnaController.swift
+//  instagram3
+//
+//  Created by FIRAT DEMİR on 18.05.2024.
+//
+
+import Foundation
---
+++ instagram3/AnaGorunum/AnapaylasımCell.swift
@@ -0,0 +1,8 @@
+//
+//  AnapaylasımCell.swift
+//  instagram3
+//
+//  Created by FIRAT DEMİR on 18.05.2024.
+//
+
+import Foundation
